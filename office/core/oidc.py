cfc0687ee9178e7fd8eac0f5f3b6c6a1d69c1180
Sandro Pischinger <mail@sandropischinger.de>
2023-12-04 15:14:29 +0100

    hack groups from id_token into user_info claims

--- office/core/oidc.py
+++ office/core/oidc.py
@@ -1,5 +1,9 @@
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from core.models import User
+from josepy.b64 import b64decode
+from josepy.b64 import b64decode
+import requests
+import json
 
 
 class MyOIDCAB(OIDCAuthenticationBackend):
@@ -31,6 +35,28 @@ class MyOIDCAB(OIDCAuthenticationBackend):
         except User.DoesNotExist:
             return self.UserModel.objects.none()
 
+    def get_userinfo(self, access_token, id_token, payload):
+        """Return user details dictionary. The id_token and payload are not used in
+        the default implementation, but may be used when overriding this method"""
+
+        user_response = requests.get(
+            self.OIDC_OP_USER_ENDPOINT,
+            headers={"Authorization": "Bearer {0}".format(access_token)},
+            verify=self.get_settings("OIDC_VERIFY_SSL", True),
+            timeout=self.get_settings("OIDC_TIMEOUT", None),
+            proxies=self.get_settings("OIDC_PROXY", None),
+        )
+        _, payload_data, _ = id_token.split(".")
+        payload_data = b64decode(payload_data)
+        payload_data = json.loads(payload_data.decode('utf-8'))
+        groups = payload_data.get('groups', [])
+        user_response.raise_for_status()
+        return {
+                'groups': groups,
+                **user_response.json()
+                }
+
     def verify_claims(self, claims):
+        print(claims)
         verified = super(MyOIDCAB, self).verify_claims(claims)
         return verified
